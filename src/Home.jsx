80889e3146d8eaf3f762f0706263522b26469aab
Suraj Kushwaha <98206380+sarickam@users.noreply.github.com>
2023-04-14 09:41:59 +0530

    edit home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,47 +1,19 @@
-import react, { useEffect, useState } from 'react'
-import { FiSun, FiMoon, FiFacebook, FiInstagram, FiYoutube, FiTwitter, FiArrowRightCircle, FiPlay, FiPhoneCall } from "react-icons/fi"
+import React from 'react'
+import { FiSun, FiFacebook, FiInstagram, FiYoutube, FiTwitter, FiArrowRightCircle, FiPhoneCall } from "react-icons/fi"
 import "./Css/styles.css"
+import Images from './images'
 import video from "./assets/video/video.mp4"
-import imgp from "./assets/img/imgp.jpg"
-import home1 from "./assets/img/home1.jpg"
-import hotel1 from "./assets/img/hotels/hotel1.jpg"
-import hotel2 from "./assets/img/hotels/hotel2.jpg"
-import al from "./assets/img/adventure/al.jpg"
-import dl from "./assets/img/adventure/dl.jpg"
-import eod from "./assets/img/adventure/eod.jpg"
-import wow from "./assets/img/adventure/wow.jpg"
-import red_fort from "./assets/img/monuments/red_fort.jpg"
-import qutub_minar from "./assets/img/monuments/qutub_minar.jpg"
-import india_gate from "./assets/img/monuments/india_gate.jpg"
-import lotus_temple from "./assets/img/monuments/lotus_temple.jpg"
-import akshar from "./assets/img/monuments/akshar.jpg"
-import police from "./assets/img/emergency/police.jpg"
-import cyber from "./assets/img/emergency/cyber.jpg"
-import fire from "./assets/img/emergency/fire.jpg"
-import ambulance from "./assets/img/emergency/ambulance.jpg"
-import women from "./assets/img/emergency/women.jpg"
-import child from "./assets/img/emergency/child.jpg"
+import { AiFillWechat } from "react-icons/ai"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel'
 import "./Css/swiper.css"
-import { Swiper, SwiperSlide } from 'swiper/react'
+// import { Swiper, SwiperSlide } from 'swiper/react'
 
 // Import Swiper styles
 import 'swiper/css'
 import Navbar from './Navbar'
 const Home = () =>{
-const [theme,settheme] = useState("light-theme")
-const [name, setname] = useState(<FiSun size={20} />)
 
-const toggledark = () => {
-    theme === "dark-theme" ? settheme("light-theme") : settheme("dark-theme")
-    theme === "dark-theme" ? setname(<FiMoon size={20} />) : setname(<FiSun size={20} />)
-
-}
-localStorage.setItem('selected-theme', theme)
-localStorage.setItem('selected-icon', name)
-useEffect(()=>{
-    document.body.className= theme
-
-},[theme])
     return(
         <>
         {/* <!-- ----------------------------  Navigation ---------------------------------------------- --> */}
@@ -54,7 +26,45 @@ useEffect(()=>{
         <main className="main">
           
             <section className="home" id="home">
-                <img src={home1} alt="" className="home__img"/>
+                <div style={{position:'absolute'}}>
+            <Carousel>
+                
+                <div>
+                    <img src={Images.slider1} alt='' />
+                   
+                </div>
+                <div>
+                    <img src={Images.slider2} alt='' />
+                   
+                </div>
+                <div>
+                    <img src={Images.slider9} alt='' />
+                   
+                </div>
+                <div>
+                    <img src={Images.red_fort} alt='' />
+                   
+                </div>
+                <div>
+                    <img src={Images.qutub_minar} alt='' />
+                   
+                </div>
+                <div>
+                    <img src={Images.lotus_temple} alt='' />
+                   
+                </div>
+                <div>
+                    <img src={Images.india_gate} alt='' />
+                   
+                </div>
+                <div>
+                    <img src={Images.akshar} alt='' />
+                   
+                </div>
+                
+            </Carousel>
+            </div>
+                {/* <img src={home1} alt="" className="home__img"/> */}
 
                 <div className="home__container container grid">
                     <div className="home__data">
@@ -79,13 +89,13 @@ useEffect(()=>{
                     <div className="home__info">
                         <div>
                             <span className="home__info-title">5 best places to visit</span>
-                            <a href="https://www.herzindagi.com/destination/top-5-tourist-places-to-visit-in-delhi-article-210447" target='_blank' className="button button--flex button--link home__info-button">
+                            <a href="#place" rel='noreferrer' className="button button--flex button--link home__info-button">
                                 More <i className="ri-arrow-right-line"> <FiArrowRightCircle size={15}/></i>
                             </a>
                         </div>
 
                         <div className="home__info-overlay">
-                            <img src={imgp} alt="" className="home__info-img"/>
+                            <img src={Images.imgp} alt="" className="home__info-img"/>
                         </div>
                     </div>
                 </div>
@@ -100,16 +110,39 @@ useEffect(()=>{
                             prices with special discounts, you choose the place we will guide you all the way to wait, get your 
                             place now.
                         </p>
-                        <a href="https://www.makemytrip.com/hotels/" target='_blank' className="button">Reserve a place</a>
+                        <a href="https://www.makemytrip.com/hotels/" target='_blank' rel='noreferrer' className="button">Reserve a place</a>
                     </div>
 
                     <div className="about__img">
                         <div className="about__img-overlay">
-                            <img src={hotel1} alt="" className="about__img-one"/>
+                            <img src={Images.hotel1} alt="" className="about__img-one"/>
                         </div>
 
                         <div className="about__img-overlay">
-                            <img src={hotel2} alt="" className="about__img-two"/>
+                            <img src={Images.hotel2} alt="" className="about__img-two"/>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <br/><br/>
+            <section className="about section" id="about">
+                <div className="about__container container grid">
+                    <div className="about__data">
+                        <h2 className="section__title about__title">More Information <br/> About The Best Hotels</h2>
+                        <p className="about__description">You can find the most beautiful and pleasant places at the best 
+                            prices with special discounts, you choose the place we will guide you all the way to wait, get your 
+                            place now.
+                        </p>
+                        <a href="https://www.makemytrip.com/hotels/" target='_blank' rel='noreferrer' className="button">Reserve a place</a>
+                    </div>
+
+                    <div className="about__img">
+                        <div className="about__img-overlay">
+                            <img src={Images.hotel1} alt="" className="about__img-one"/>
+                        </div>
+
+                        <div className="about__img-overlay">
+                            <img src={Images.hotel2} alt="" className="about__img-two"/>
                         </div>
                     </div>
                 </div>
@@ -121,7 +154,7 @@ useEffect(()=>{
                 <div className="place__container container grid">
                    
                     <div className="place__card">
-                        <img src={red_fort} alt="" className="place__img"/>
+                        <img src={Images.red_fort} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -136,13 +169,13 @@ useEffect(()=>{
                         </div>
 
                         <button className="button button--flex place__button" >
-                        <i className="ri-arrow-right-line"> <a href='https://en.wikipedia.org/wiki/Red_Fort' target='_blank'> <FiArrowRightCircle size={15}/></a></i>
+                        <i className="ri-arrow-right-line"> <a href='https://en.wikipedia.org/wiki/Red_Fort' target='_blank' rel='noreferrer'> <FiArrowRightCircle size={15}/></a></i>
                         </button>
                     </div>
 
                    
                     <div className="place__card">
-                        <img src={india_gate} alt="" className="place__img"/>
+                        <img src={Images.india_gate} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -157,13 +190,13 @@ useEffect(()=>{
                         </div>
 
                         <button className="button button--flex place__button">
-                            <i className="ri-arrow-right-line"> <a href='https://en.wikipedia.org/wiki/India_Gate' target='_blank'><FiArrowRightCircle size={15}/></a></i>
+                            <i className="ri-arrow-right-line"> <a href='https://en.wikipedia.org/wiki/India_Gate' target='_blank' rel='noreferrer'><FiArrowRightCircle size={15}/></a></i>
                         </button>
                     </div>
 
                    
                     <div className="place__card">
-                        <img src={qutub_minar} alt="" className="place__img"/>
+                        <img src={Images.qutub_minar} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -178,13 +211,13 @@ useEffect(()=>{
                         </div>
 
                         <button className="button button--flex place__button">
-                            <i className="ri-arrow-right-line"> <a href='https://en.wikipedia.org/wiki/Qutb_Minar' target='_blank'> <FiArrowRightCircle size={15}/></a></i>
+                            <i className="ri-arrow-right-line"> <a href='https://en.wikipedia.org/wiki/Qutb_Minar' target='_blank' rel='noreferrer'> <FiArrowRightCircle size={15}/></a></i>
                         </button>
                     </div>
 
                    
                     <div className="place__card">
-                        <img src={akshar} alt="" className="place__img"/>
+                        <img src={Images.akshar} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -205,7 +238,7 @@ useEffect(()=>{
 
                    
                     <div className="place__card">
-                        <img src={lotus_temple} alt="" className="place__img"/>
+                        <img src={Images.lotus_temple} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -220,7 +253,7 @@ useEffect(()=>{
                         </div>
 
                         <button className="button button--flex place__button">
-                            <i className="ri-arrow-right-line"> <a href='https://en.wikipedia.org/wiki/Lotus_Temple' target='_blank'><FiArrowRightCircle size={15}/></a></i>
+                            <i className="ri-arrow-right-line"> <a href='https://en.wikipedia.org/wiki/Lotus_Temple' target='_blank' rel='noreferrer'><FiArrowRightCircle size={15}/></a></i>
                         </button>
                     </div>
                 </div>
@@ -258,22 +291,22 @@ useEffect(()=>{
 
                     <div className="experience__img grid">
                         <div className="experience__overlay">
-                            <a href='https://adventureisland.in/travel/' target='_blank'>
-                            <img src={al} alt="" className="experience__img-one"/></a>
+                            <a href='https://adventureisland.in/travel/' target='_blank' rel='noreferrer'>
+                            <img src={Images.al} alt="" className="experience__img-one"/></a>
                         </div>
                         
                         <div className="experience__overlay">
-                        <a href='http://www.drizzlingland.com/' target='_blank'>
-                            <img src={dl} alt="" className="experience__img-two"/></a>
+                        <a href='http://www.drizzlingland.com/' target='_blank' rel='noreferrer'>
+                            <img src={Images.dl} alt="" className="experience__img-two"/></a>
                         </div>
                         
                         <div className="experience__overlay">
-                        <a href='https://www.eodindia.com/' target='_blank'>
-                            <img src={wow} alt="" className="experience__img-three"/></a>
+                        <a href='https://www.eodindia.com/' target='_blank' rel='noreferrer'>
+                            <img src={Images.wow} alt="" className="experience__img-three"/></a>
                         </div>
                         <div className="experience__overlay">
-                        <a href='https://www.wastetowonderpark.com/' target='_blank'>
-                            <img src={eod} alt="" className="experience__img-four"/></a>
+                        <a href='https://www.wastetowonderpark.com/' target='_blank' rel='noreferrer'>
+                            <img src={Images.eod} alt="" className="experience__img-four"/></a>
                         </div>
                     </div>
                 </div>
@@ -296,13 +329,13 @@ useEffect(()=>{
             </section>
 
            
-            <section className="place section" id="place">
+            <section className="place section" id="emergency">
                 <h2 className="section__title">Emergency Contact</h2>
 
                 <div className="place__container container grid">
                    
                     <div className="place__card">
-                        <img src={police} alt="" className="place__img"/>
+                        <img src={Images.police} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -322,7 +355,7 @@ useEffect(()=>{
 
                    
                     <div className="place__card">
-                        <img src={cyber} alt="" className="place__img"/>
+                        <img src={Images.cyber} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -341,7 +374,7 @@ useEffect(()=>{
 
                    
                     <div className="place__card">
-                        <img src={fire} alt="" className="place__img"/>
+                        <img src={Images.fire} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -360,7 +393,7 @@ useEffect(()=>{
 
                    
                     <div className="place__card">
-                        <img src={ambulance} alt="" className="place__img"/>
+                        <img src={Images.ambulance} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -379,7 +412,7 @@ useEffect(()=>{
 
                    
                     <div className="place__card">
-                        <img src={women} alt="" className="place__img"/>
+                        <img src={Images.women} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -396,7 +429,7 @@ useEffect(()=>{
                         </button>
                     </div>
                     <div className="place__card">
-                        <img src={child} alt="" className="place__img"/>
+                        <img src={Images.child} alt="" className="place__img"/>
                         
                         <div className="place__content">
                             <span className="place__rating">
@@ -513,14 +546,18 @@ useEffect(()=>{
                     <div className="footer__terms">
                         <a href="#/" className="footer__terms-link">Terms & Agreements</a>
                         <a href="#/" className="footer__terms-link">Privacy Policy</a>
+                        
                     </div>
                 </div>
+            </div>
+            <div style={{position:"fixed",bottom:"15px",right:"60px",cursor:"pointer"}}>
+                <i ><a href='#emergency'> <FiPhoneCall size={50} color='red'/> </a></i>
             </div>
         </footer>
 
         
         <a href="#/" className="scrollup" id="scroll-up">
-            <i className="ri-arrow-up-line scrollup__icon"></i>
+            <i className="ri-arrow-up-line scrollup__icon"><FiSun size={50}/></i>
         </a>
         </>
     )
---
+++ src/images.js
@@ -0,0 +1,33 @@
+const Images={
+slider1: require("./assets/img/monuments/Agrasen-Ki-Baoli-1-2-768x492.jpg"),
+slider2: require("./assets/img/monuments/Chattarpur_Temple.jpg"),
+slider3: require("./assets/img/monuments/Hauz-Khas.jpg"),
+slider4: require("./assets/img/monuments/Jantar-Mantar_1400-1000x565.jpeg"),
+slider5: require("./assets/img/monuments/Purana-Qila-Delhi.jpg"),
+slider6: require("./assets/img/monuments/The formal gardens today_PHOTO CREDIT - Narendra Bisht courtesy Sahapedia(dot)org.jpg"),
+slider7: require("./assets/img/monuments/bangla.jpg"),
+slider8: require("./assets/img/monuments/d037496bdc8b72cf1e78bd9a08a5a823-lodi-gardens.jpg"),
+slider9: require("./assets/img/monuments/jama-masjid-delhi.jpg"),
+
+hotel1 : require( "./assets/img/hotels/hotel1.jpg"),
+hotel2 : require( "./assets/img/hotels/hotel2.jpg"),
+al : require( "./assets/img/adventure/al.jpg"),
+dl : require( "./assets/img/adventure/dl.jpg"),
+eod : require( "./assets/img/adventure/eod.jpg"),
+wow : require( "./assets/img/adventure/wow.jpg"),
+red_fort: require( "./assets/img/monuments/red_fort.jpg"),
+qutub_minar : require( "./assets/img/monuments/qutub_minar.jpg"),
+india_gate : require( "./assets/img/monuments/india_gate.jpg"),
+lotus_temple : require( "./assets/img/monuments/lotus_temple.jpg"),
+akshar : require( "./assets/img/monuments/akshar.jpg"),
+bangla : require( "./assets/img/monuments/bangla.jpg"),
+police : require( "./assets/img/emergency/police.jpg"),
+cyber : require( "./assets/img/emergency/cyber.jpg"),
+fire : require( "./assets/img/emergency/fire.jpg"),
+ambulance : require( "./assets/img/emergency/ambulance.jpg"),
+women : require( "./assets/img/emergency/women.jpg"),
+child : require( "./assets/img/emergency/child.jpg"),
+imgp: require("./assets/img/imgp.jpg"),
+}
+export default Images
+
